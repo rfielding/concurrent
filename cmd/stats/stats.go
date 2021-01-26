649eb3b7eeb77b247862208d7c1261e6bdebfaeb
rfielding <rfielding@pop-os.localdomain>
2021-01-26 08:59:53 -0500

    still working on gd

--- cmd/stats/stats.go
+++ cmd/stats/stats.go
@@ -29,13 +29,13 @@ type Reporter struct {
 	Gamma          float64
 }
 
-// sum_{n}{ (X_n - X[n])^2 }/N
+// sum_{n}{ (X[n] - X_n)^2 }/N
 func (r *Reporter) errf(da, db, dg float64, throughputByLoad []float64) float64 {
 	err := float64(0)
         count := int(0)
 	for ni := 1; ni < len(throughputByLoad); ni++ {
 		n := int64(ni)
-		dist := throughputByLoad[n] - r.X(da, db, dg, n)
+		dist := r.X(da,db,dg,n) - throughputByLoad[n]
 		err += dist * dist
 		count++
 	}
@@ -43,12 +43,11 @@ func (r *Reporter) errf(da, db, dg float64, throughputByLoad []float64) float64 
 }
 
 // Point in the direction you must go to IMPROVE
-// -grad sum_{n}{ (X_n - X[n])^2 }/N
-// -sum_{n} { grad (X_n - X[n])^2 }/N
-// -sum_{n} { 2 (X_n - X[n])(grad (X_n - X[n])) }/N
-// -sum_{n} { 2 (X_n - X[n])(-grad X[n]) }/N
-// -sum_{n} { -2 (X_n - X[n])(grad X[n]) }/N
-// -1/N * sum_{n} { -2 (X_n - X[n]) * (dXa, dXb, dXg) }
+// -grad sum_{n}{ (X[n] - X_n)^2 }/N
+// -sum_{n} { grad (X[n] - X_n)^2 }/N
+// -sum_{n} { 2 (X[n] - X_n)(grad (X_n - X[n])) }/N
+// -sum_{n} { 2 (X[n] - X_n)(-grad X[n]) }/N
+// -1/N * sum_{n} { (X[n] - X_n) * (dXa, dXb, dXg) }
 func (r *Reporter) gradErrf(throughputByLoad []float64) (float64, float64, float64) {
 	da := float64(0)
 	db := float64(0)
@@ -56,13 +55,13 @@ func (r *Reporter) gradErrf(throughputByLoad []float64) (float64, float64, float
         count := int(0)
 	for ni := 1; ni < len(throughputByLoad); ni++ {
 		n := int64(ni)
-		dist := (throughputByLoad[n] - r.X(0,0,0,n))
-		da += 2 * dist * r.dXa(n)
-		db += 2 * dist * r.dXb(n)
-		dg += 2 * dist * r.dXg(n)
+		dist := r.X(0,0,0,n) - throughputByLoad[n]
+		da += dist * r.dXa(n)
+		db += dist * r.dXb(n)
+		dg += dist * r.dXg(n)
 		count++
 	}
-	return da/float64(count), db/float64(count), dg/float64(count)
+	return -da/float64(count), -db/float64(count), -dg/float64(count)
 }
 
 // dXa = -(n g) (n-1) (1 + a (n-1) + b n (n-1))^{-2}
